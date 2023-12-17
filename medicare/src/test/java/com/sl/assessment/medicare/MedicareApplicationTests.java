4ba922701e2e1231c05101ae9251ada609a48e51
hirahul0011 <ra_10001@yahoo.co.in>
2023-12-17 17:05:11 +0530

    springBoot base project setup is done

---
+++ medicare/src/test/java/com/sl/assessment/medicare/MedicareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sl.assessment.medicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
