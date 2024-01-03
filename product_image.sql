0aa28277f7adb675dd5c7138466899f861fc3b09
hirahul0011 <ra_10001@yahoo.co.in>
2024-01-03 10:38:57 +0530

    Additional SQL file added

---
+++ product_image.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `product_image` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `product_product_image` longblob,
+  `product_product_image_name` varchar(255) DEFAULT NULL,
+  `product_product_image_type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30008 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+/*
+-- Query: SELECT * FROM medicare.product_image
+LIMIT 0, 1000
+
+-- Date: 2024-01-03 10:35
+*/
+INSERT INTO `` (`product_id`,`product_product_image`,`product_product_image_name`,`product_product_image_type`) VALUES (30001,?,'lotus 1670598623881.jpeg','image/jpeg');
+INSERT INTO `` (`product_id`,`product_product_image`,`product_product_image_name`,`product_product_image_type`) VALUES (30002,?,'Image2','JPG');
+INSERT INTO `` (`product_id`,`product_product_image`,`product_product_image_name`,`product_product_image_type`) VALUES (30003,?,'Image3','JPG');
+INSERT INTO `` (`product_id`,`product_product_image`,`product_product_image_name`,`product_product_image_type`) VALUES (30004,?,'Image4','JPG');
+INSERT INTO `` (`product_id`,`product_product_image`,`product_product_image_name`,`product_product_image_type`) VALUES (30005,?,'Image5','JPG');
+INSERT INTO `` (`product_id`,`product_product_image`,`product_product_image_name`,`product_product_image_type`) VALUES (30006,?,'Lotus.PNG','image/png');
+INSERT INTO `` (`product_id`,`product_product_image`,`product_product_image_name`,`product_product_image_type`) VALUES (30007,?,'Lotus.PNG','image/png');
